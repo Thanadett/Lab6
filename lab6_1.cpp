017427da822ae2de5e72671b58726073aa04280f
Thanadett <tanapremin@gmail.com>
2023-12-01 15:06:36 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-    int x;
+    int integer;
 
     cout << "Enter an integer: ";
-    cin >> x;
+    cin >> integer;
 
     int even = 0, odd = 0;
-    while (x != 0)
+    while (integer != 0)
     {
-        if (x % 2 == 0)
+        if (integer % 2 == 0)
         {
             even += 1;
         }
@@ -20,7 +20,7 @@ int main()
             odd += 1;
         }
         cout << "Enter an integer: ";
-        cin >> x;
+        cin >> integer;
     }
     cout << "#Even numbers = " << even << endl;
     cout << "#Odd numbers = " << odd;
